3112f18815ceb352e732a84e2a4654cf53b4f0a6
risnanto73 <tiolast63@gmail.com>
2024-09-12 12:59:34 +0700

    update

--- lib/ui/food_page.dart
+++ lib/ui/food_page.dart
@@ -8,7 +8,6 @@ class FoodPage extends StatefulWidget {
 }
 
 class _FoodPageState extends State<FoodPage> {
-
   int selectedIndex = 0;
 
   @override
@@ -23,10 +22,77 @@ class _FoodPageState extends State<FoodPage> {
           padding: EdgeInsets.symmetric(horizontal: defaultMargin),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Food Market",
+                    style: blackFontStyle1,
+                  ),
+                  Text(
+                    "Let's get some foods",
+                    style: blackFontStyle2,
+                  ),
+                ],
+              ),
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      'https://i.pinimg.com/736x/c8/4b/1b/c84b1bc7fb9fe438e9ac111af9db1b94.jpg',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
         //Card Food
+        Container(
+          height: 220,
+          width: double.infinity,
+          margin: EdgeInsets.symmetric(
+            vertical: defaultMargin,
+          ),
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: mockFoods
+                .map(
+                  (food) => Padding(
+                    padding: EdgeInsets.only(
+                      left: (food == mockFoods.first) ? defaultMargin : 0,
+                      right: defaultMargin,
+                    ),
+                    child: FoodCard(food: food),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
         //Tab Layout
+        Container(
+          width: double.infinity,
+          color: Colors.white,
+          child: Column(
+            children: [
+              CustomTabbar(
+                  selectedIndex: selectedIndex,
+                  titles: [
+                    'New Taste',
+                    'Popular',
+                    'Recommended',
+                  ],
+                  onTap: (index) {}),
+              SizedBox(height: 20),
+            ],
+          ),
+        )
       ],
     );
   }
